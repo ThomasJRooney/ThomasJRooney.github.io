905f41a9165710cca3a4606aa456c7edd0bbdce7
Thomas Rooney <roone194@umn.edu>
2021-05-29 15:04:50 -0500

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,4 +26,22 @@ $(document).ready(function() {
 		$('.menu').removeClass('open');
 		event.stopPropagation();
 	});
+  function openGallery(id) {
+  closeAll();
+  const gallery = document.getElementById('gallery-'+id);
+  const card = document.getElementById('card-'+id);
+  gallery.classList.add('Gallery--active');
+  card.classList.add('Card--active');
+}
+
+function closeAll() {
+  const galleryActv = document.querySelector('.Gallery--active');
+  const cardActv = document.querySelector('.Card--active');
+  if (galleryActv) {
+    galleryActv.classList.remove('Gallery--active');
+  }
+  if (cardActv) {
+    cardActv.classList.remove('Card--active');
+  }
+}
 });
